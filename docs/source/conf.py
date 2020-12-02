233dbc80b0c39b21ed086261307bbaf1852c4359
44050496 <ra.redmer@outlook.com>
2020-12-02 21:53:12 +0100

    Mock up external modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,3 +56,6 @@ master_doc = 'index'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Skip specified package installations
+autodoc_mock_imports = ["numpy", "sklearn"]
